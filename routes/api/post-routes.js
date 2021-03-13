f0e23211dfa948555feed455968b1cf83d4f9d34
Olivia Faraone <oliviafaraone1@MacBook-Pro-10.local>
2021-03-13 16:55:18 -0700

    save

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -38,7 +38,7 @@ router.get('/:id', (req, res) => {
     },
     attributes: ['id', 'post_url', 'title', 'created_at', [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']],
     include: [
-      {
+      { 
         model: User,
         attributes: ['username']
       }
