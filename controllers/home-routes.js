3847edaeb9e15a3207d785f48444bef41c6ee312
Olivia Faraone <oliviafaraone1@MacBook-Pro-10.local>
2021-03-31 09:26:39 -0700

    logout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,7 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+    console.log(req.session);
     Post.findAll({
         attributes: [
           'id',
@@ -27,9 +28,6 @@ router.get('/', (req, res) => {
         ]
       })
         .then(dbPostData => {
-          // pass a single post object into the homepage template
-          //console.log(dbPostData[0]);
-          //res.render('homepage', dbPostData[0].get({ plain: true }));
           const posts = dbPostData.map(post => post.get({ plain: true }));
           res.render('homepage', { posts });
         })
@@ -39,4 +37,13 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+      }
+    
+      res.render('login');
+  });
+   
 module.exports = router;
