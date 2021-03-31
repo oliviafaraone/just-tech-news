d6fea7cea601730cf9001362f2729e4866627298
Olivia Faraone <oliviafaraone1@MacBook-Pro-10.local>
2021-03-31 15:16:30 -0700

    edit post

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -47,7 +47,7 @@ async function signupFormHandler(event) {
       });
   
       if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
